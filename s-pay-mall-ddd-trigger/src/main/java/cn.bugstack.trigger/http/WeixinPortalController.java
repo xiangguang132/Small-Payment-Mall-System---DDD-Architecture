fad4d59ac6fe2d9ff3c973a55a84f1f5b52bcc15
cc_132678 <1326784764@qq.com>
2026-07-24 14:33:05 +0800

    feat:测试微信扫码登录成功

--- s-pay-mall-ddd-trigger/src/main/java/cn.bugstack.trigger/http/WeixinPortalController.java
+++ s-pay-mall-ddd-trigger/src/main/java/cn.bugstack.trigger/http/WeixinPortalController.java
@@ -71,7 +71,8 @@ public class WeixinPortalController {
             }
 
             log.info("接收微信公众号信息请求{}完成 {}", openid, requestBody);
-            return buildMessageTextEntity(openid, "测试本案例，需要请扫码登录！");
+//            return buildMessageTextEntity(openid, "测试本案例，需要请扫码登录！");
+            return buildMessageTextEntity(openid, message.getContent().toString());
         } catch (Exception e) {
             log.error("接收微信公众号信息请求{}失败 {}", openid, requestBody, e);
             return "";
