ef8410cdc1b6e5bdfd2f3c05cdbc0cdfe3b6ad86
cc_132678 <1326784764@qq.com>
2026-07-26 20:25:50 +0800

    feat:添加定时任务

--- s-pay-mall-ddd-trigger/src/main/java/cn.bugstack.trigger/http/AliPayController.java
+++ s-pay-mall-ddd-trigger/src/main/java/cn.bugstack.trigger/http/AliPayController.java
@@ -7,12 +7,15 @@ import cn.bugstack.domain.order.model.entity.PayOrderEntity;
 import cn.bugstack.domain.order.model.entity.ShopCartEntity;
 import cn.bugstack.domain.order.service.IOrderService;
 import cn.bugstack.types.enums.ResponseCode;
+import com.alipay.api.internal.util.AlipaySignature;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
+import java.util.HashMap;
+import java.util.Map;
 
 @Slf4j
 @RestController
@@ -51,8 +54,44 @@ public class AliPayController implements IPayService {
         }
     }
 
-//    @RequestMapping(value = "pay_notify", method = RequestMethod.POST)
-//    public String payNotify(HttpServletRequest request) {
-//
-//    }
+    @RequestMapping(value = "pay_notify", method = RequestMethod.POST)
+    public String payNotify(HttpServletRequest request) {
+        try {
+            log.info("支付回调，消息接收 {}", request.getParameter("trade_status"));
+            if (request.getParameter("trade_status").equals("TRADE_SUCCESS")) {
+                Map<String, String> params = new HashMap<>();
+                Map<String, String[]>  requestParams = request.getParameterMap();
+                for (String key : requestParams.keySet()) {
+                    params.put(key, request.getParameter(key));
+                }
+
+                String out_trade_no = params.get("out_trade_no");
+                String gmt_create = params.get("gmt_create");
+                String trade_no = params.get("trade_no");
+
+                String sign = request.getParameter("sign");
+                String content = AlipaySignature.getSignCheckContentV1(params);
+                boolean checkSignature = AlipaySignature.rsa256CheckContent(content, sign, alipayPublicKey, "UTF-8"); // 验证签名
+                // 支付宝验签
+                if (checkSignature) {
+                    // 验签通过
+                    log.info("支付回调，交易名称: {}", params.get("subject"));
+                    log.info("支付回调，交易状态: {}", params.get("trade_status"));
+                    log.info("支付回调，支付宝交易凭证号: {}", params.get("trade_no"));
+                    log.info("支付回调，商户订单号: {}", params.get("out_trade_no"));
+                    log.info("支付回调，交易金额: {}", params.get("total_amount"));
+                    log.info("支付回调，买家在支付宝唯一id: {}", params.get("buyer_id"));
+                    log.info("支付回调，买家付款时间: {}", params.get("gmt_payment"));
+                    log.info("支付回调，买家付款金额: {}", params.get("buyer_pay_amount"));
+                    log.info("支付回调，支付回调，更新订单 {}", out_trade_no);
+                    // 更新订单已支付
+                    orderService.changeOrderPaySuccess(out_trade_no);
+                }
+            }
+            return "success";
+        } catch (Exception e) {
+            log.error("支付回调，处理失败", e);
+            return "false";
+        }
+    }
 }
