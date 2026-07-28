b12896971b79c123d555757c982b53ced9969a06
cc_132678 <1326784764@qq.com>
2026-07-28 11:17:52 +0800

    feat:初始化pom依赖，添加yml配置文件

--- s-pay-mall-ddd-app/src/main/java/cn/bugstack/app/Application.java
+++ s-pay-mall-ddd-app/src/main/java/cn/bugstack/app/Application.java
@@ -1,4 +1,17 @@
 package cn.bugstack.app;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@SpringBootApplication
+@Configuration
+@EnableScheduling
 public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+        System.out.println("hello world");
+    }
 }
