a0425a73a28e4605778ce48554c46fc242187321
cc_132678 <1326784764@qq.com>
2026-08-01 20:45:56 +0800

    feat:-添加依据status状态查询物料库存分配单数据2-

---
+++ s-pay-mall-ddd-api/src/main/java/cn/bugstack/api/response/materialstockallocation/MaterialStockAllocationStatusResponse.java
@@ -0,0 +1,25 @@
+package cn.bugstack.api.response.materialstockallocation;
+
+import lombok.Builder;
+import lombok.Data;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+public class MaterialStockAllocationStatusResponse {
+
+    private Long id;
+    private String allocationNo;
+    private Long materialId;
+    private Long requestStockId;
+    private BigDecimal requestQty;
+    private BigDecimal lockedQty;
+    private BigDecimal outboundQty;
+    private BigDecimal releasedQty;
+    private Integer status;
+//    private String reason;
+//    private LocalDateTime createTime;
+//    private LocalDateTime updateTime;
+}
